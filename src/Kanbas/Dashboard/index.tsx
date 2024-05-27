d87bc7c89680a8950dd26531f4dd7de3a91dc4a6
iris19951229 <shaonian1229@gmail.com>
2024-05-26 23:16:56 -0700

    a3 JavaScript update

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,27 +1,31 @@
 import { Link } from "react-router-dom";
 import * as db from "../Database";
+import "./index.css";
 export default function Dashboard() {
-  const courses = db.courses;
+  const courses = db.courses
   return (
-    <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+    <div id="wd-dashboard" className="container-fluid">
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
+      <hr />
       <div id="wd-dashboard-courses" className="row">
-        <div className="row row-cols-1 row-cols-md-5 g-4">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
           {courses.map((course) => (
-            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-              <Link to={`/Kanbas/Courses/${course._id}/Home`} className="text-decoration-none" >
-                <div className="card rounded-3 overflow-hidden">
-                  <img src={course.image} height="{160}" />
-                  <div className="card-body">
-                    <span className="wd-dashboard-course-link"
-                      style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }} >
+            <div key={course._id} className="col">
+              <Link to={`/Kanbas/Courses/${course._id}/Home`} className="text-decoration-none">
+                <div className="card rounded-3 overflow-hidden h-100">
+                  <img src={course.image} className="card-img-top" alt={course.name} style={{ height: '160px', objectFit: 'cover' }} />
+                  <div className="card-body d-flex flex-column">
+                    <span className="wd-dashboard-course-link mb-2" style={{ textDecoration: "none", color: "navy", fontWeight: "bold" }}>
                       {course.name}
                     </span>
-                    <p className="wd-dashboard-course-title card-text" style={{ maxHeight: 53, overflow: "hidden" }}>
+                    <p className="wd-dashboard-course-title card-text mb-4" style={{ maxHeight: '53px', overflow: 'hidden' }}>
                       {course.description}
                     </p>
-                    <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                    <div>
+                     <Link to={`/Kanbas/Courses/${course._id}/Home`} className="btn btn-primary">Go</Link>
+                    </div>
                   </div>
                 </div>
               </Link>
