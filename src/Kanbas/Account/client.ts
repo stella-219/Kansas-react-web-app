464f3fff1a7c9ed12a1fb73b77abc577382db53c
iris19951229 <shaonian1229@gmail.com>
2024-06-18 13:44:36 -0700

    fix local server

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
-export const REMOTE_SERVER = "https://kanbas-node-server-app-2-ifid.onrender.com"
+export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post( `${USERS_API}/signin`, credentials );
   return response.data;
 };
 export const profile = async () => {
-  const response = await axiosWithCredentials.post(`https://kanbas-node-server-app-2-ifid.onrender.com/api/users/profile`);
+  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
   return response.data;
 };
 export const signup = async (user: any) => {
